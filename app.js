e452aa8b3dccfd16b4027c74289f1e4ac4544d5c
judy3578 <hyejoo98@g.skku.edu>
2022-03-02 22:51:31 +0900

    js study1

---
+++ app.js
@@ -0,0 +1,31 @@
+// 변수
+const a = 10;
+const b = 2;
+let myName = "hyejoo";
+
+console.log(a + b)
+console.log(a * b)
+console.log(a / b)
+console.log("hello "+myName)
+
+myName = "joojoo"
+console.log("hello "+myName)
+
+// 데이터 타입
+const amIFat = null;
+console.log(amIFat);
+
+const amIFat = false;
+console.log(amIFat);
+
+let something;  //값을 주지 않은 undefined 값
+console.log(something);
+
+// 배열
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+
+// Get Item from Array
+console.log(daysOfWeek);
+
+daysOfWeek.push("sun");
+console.log(daysOfWeek);
